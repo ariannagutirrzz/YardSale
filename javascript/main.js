b426efe02c6a407aaa91f08b3a897336bde44f00
Arianna Vega <ariannavega2021@gmail.com>
2023-07-16 02:01:34 -0400

    mobile menu function

--- javascript/main.js
+++ javascript/main.js
@@ -2,10 +2,17 @@ const menuEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
 const flechita = document.querySelector(".flechita")
 
+const barrasMenu = document.querySelector(".barritas")
+const mobileMenu = document.querySelector(".menu-mobile")
+
 flechita.addEventListener("click", toggleMenu)
 menuEmail.addEventListener("click", toggleMenu)
-
+barrasMenu.addEventListener("click", barritas)
 
 function toggleMenu () {
     desktopMenu.classList.toggle("inactive")    
+}
+
+function barritas () {
+    mobileMenu.classList.toggle("inactive")
 }
