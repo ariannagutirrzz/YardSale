f9af402f744690822a8e3a40a3cabc59f5548121
Arianna Vega <ariannavega2021@gmail.com>
2023-07-17 18:47:56 -0400

    modal box added

--- javascript/main.js
+++ javascript/main.js
@@ -40,21 +40,25 @@ const productList = [
     name: "Camera",
     price: 120,
     img: "https://http2.mlstatic.com/D_NQ_NP_745276-MLV52320264720_112022-O.webp",
+    id: 1,
   },
   {
     name: "TV",
     price: 280,
-    img: "https://http2.mlstatic.com/D_NQ_NP_988410-MLV52575152124_112022-W.webp"
+    img: "https://http2.mlstatic.com/D_NQ_NP_988410-MLV52575152124_112022-W.webp",
+    id: 2,
   },
   {
     name: "PlayStation 5",
     price: 600,
     img: "https://http2.mlstatic.com/D_NQ_NP_887928-MLV45188629838_032021-O.webp",
+    id: 3,
   },
   {
     name: "Desktop",
     price: 300,
     img: "https://http2.mlstatic.com/D_NQ_NP_738099-MLV49532436089_032022-O.webp",
+    id: 4,
   }
 ];
 
@@ -62,7 +66,7 @@ const productList = [
 function products (arr) {
     for(product of arr) {
         const htmlCards = `<div class="product-card">
-            <img src= ${product.img}>
+            <img src= ${product.img} onclick="change()">
             <div class="product-info">
                 <div>
                     <p>$ ${product.price} </p>
@@ -81,3 +85,31 @@ function products (arr) {
 }
 
 products(productList);
+
+function change () {
+  modal.style.display = "block";
+}
+
+
+// Get the button that opens the modal
+var modal = document.getElementById("myModal");
+// When the user clicks the button, open the modal 
+
+  var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
